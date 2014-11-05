113dd57d11f29dd4c3910c3e1f2c5ef582385ef4
jasonbutler42 <jasonbutler42@gmail.com>
2014-11-05 09:23:46 -0600

    added column declarations to main and sidebar

--- web/app/themes/grillinfools-theme/base.php
+++ web/app/themes/grillinfools-theme/base.php
@@ -14,11 +14,11 @@
 
   <div class="wrap container" role="document">
     <div class="content row">
-      <main class="main" role="main">
+      <main class="main col-md-8" role="main">
         <?php include roots_template_path(); ?>
       </main><!-- /.main -->
       <?php if (roots_display_sidebar()) : ?>
-        <aside class="sidebar" role="complementary">
+        <aside class="sidebar col-md-4" role="complementary">
           <?php include roots_sidebar_path(); ?>
         </aside><!-- /.sidebar -->
       <?php endif; ?>
