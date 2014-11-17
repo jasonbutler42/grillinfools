361900fc2db62dc8bfb3451561dbf464af8b6cc3
jasonbutler42 <jasonbutler42@gmail.com>
2014-11-17 15:17:50 -0600

    switched to the smaller, optimized svg logo

--- web/app/themes/grillinfools-theme/templates/header.php
+++ web/app/themes/grillinfools-theme/templates/header.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><span class="logo"><?php include(get_template_directory() . "/assets/img/logo.svg"); ?><img class="logo-svg-alt" src="<?php echo get_template_directory_uri() . "/assets/img/logo-large.png"; ?>" alt="logo" title=""><?php bloginfo('name'); ?></span></a>
+      <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><span class="logo"><?php include(get_template_directory() . "/assets/img/logo-min.svg"); ?><img class="logo-svg-alt" src="<?php echo get_template_directory_uri() . "/assets/img/logo-large.png"; ?>" alt="logo" title=""><?php bloginfo('name'); ?></span></a>
       <div class="tagline"><?php echo get_bloginfo ( 'description' ); ?></div>
     </div>
     <nav class="collapse navbar-collapse navbar-primary" role="navigation">
@@ -39,7 +39,7 @@
   </nav>
   <nav class="navbar-category" role="navigation">
     <div class="container">
-      <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><span class="logo"><?php include(get_template_directory() . "/assets/img/logo.svg"); ?><img class="logo-svg-alt" src="<?php echo get_template_directory_uri() . "/assets/img/logo-large.png"; ?>" alt="logo" title=""><?php bloginfo('name'); ?></span></a>
+      <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><span class="logo"><?php include(get_template_directory() . "/assets/img/logo-min.svg"); ?><img class="logo-svg-alt" src="<?php echo get_template_directory_uri() . "/assets/img/logo-large.png"; ?>" alt="logo" title=""><?php bloginfo('name'); ?></span></a>
       <div class="category-navigation-wrap">
       <?php
         if (has_nav_menu('category_navigation')) :
