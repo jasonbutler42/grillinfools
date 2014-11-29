033265a81e8bf3d4894698b8c09860faa5941312
jasonbutler42 <jasonbutler42@gmail.com>
2014-11-29 13:47:58 -0600

    byline styling

--- web/app/themes/grillinfools-theme/templates/entry-meta.php
+++ web/app/themes/grillinfools-theme/templates/entry-meta.php
@@ -1,3 +1,5 @@
-
-<time class="updated" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
-<p class="byline author vcard"><?php echo __('By', 'roots'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a><br></p>
+<div class="byline">
+	<span class="meta meta-time"><?php echo __('posted', 'roots'); ?> <time class="updated" datetime="<?php echo get_the_time('c'); ?>"><i><?php echo get_the_date(); ?></i></time></span> |
+	<span class="meta meat-author vcard"><?php echo __('author', 'roots'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><i><?php echo get_the_author(); ?></i></a></span> |
+	<span class="meta meta-comment"><a href="<?php comments_link(); ?>"> <?php comments_number( '<i>no comments</i>', '<i>one</i> comment', '<i>%</i> comments' ); ?></a></span>
+</div>
