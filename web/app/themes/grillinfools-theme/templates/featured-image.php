7314b337c2198f35a342d752397e539921da8b3e
jasonbutler42 <jasonbutler42@gmail.com>
2014-12-04 07:21:41 -0600

    Release v1.10.5

--- web/app/themes/grillinfools-theme/templates/featured-image.php
+++ web/app/themes/grillinfools-theme/templates/featured-image.php
@@ -2,6 +2,8 @@
 		<?php //if (!is_category() && !is_search() && !is_archive() && !is_home()): ?>
 	        <?php if ( has_post_thumbnail() && strlen($img = get_the_post_thumbnail( get_the_ID(), array( 150, 150 ) ) ) ) : ?>
 	          <div class="featured-image" style="background-image: url('<?php echo featured_image($post->ID, 1170, 400, 60, true); ?>')"></div>
+	      <?php else : ?>
+	      		<div class="featured-image featured-image-default featured-random-<?php echo rand(1,5); ?>"></div>
         	<?php endif; ?>
     	<?php //endif; ?>
       <?php endwhile; ?>
