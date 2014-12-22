3c53e65d20639e64f6bac68b36786db3137d6dac
jasonbutler42 <jasonbutler42@gmail.com>
2014-12-21 22:49:31 -0600

    fix(pinterest.php): remove bad call to missing file

--- web/app/themes/grillinfools-theme/functions.php
+++ web/app/themes/grillinfools-theme/functions.php
@@ -21,7 +21,6 @@ $roots_includes = array(
   'lib/gallery.php',         // Custom [gallery] modifications
   'lib/scripts.php',         // Scripts and stylesheets
   'lib/extras.php',          // Custom functions
-  'lib/pinterest.php',       // Pinterest Pin It functionality for images
   'lib/comments.php',        // Custom comments walker
 );
 
