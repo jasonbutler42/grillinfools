b3496e3f4a456955cf1e45752540659c6b18c6ca
jasonbutler42 <jasonbutler42@gmail.com>
2015-01-16 16:40:02 -0600

    fix(tastytreats): honeypot change
    - Removed display:none; to make it visible to DOM
    - added .text-hide(); to keep it from humans

--- web/app/themes/grillinfools-theme/templates/footer.php
+++ web/app/themes/grillinfools-theme/templates/footer.php
@@ -2,6 +2,6 @@
   	<div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
   	</div>
-	<div style="display: none;"><a href="http://grillinfools.com/dumbass_robots/sosweet.php">Bear</a></div>
+	<div class="tastytreats"><a href="http://grillinfools.com/dumbass_robots/sosweet.php">Bear</a></div>
   	<a href="/dumbass_robots/" rel="nofollow"><img src="/dumbass_robots/dumbass_robots.gif" alt=""></a>
 </footer>
