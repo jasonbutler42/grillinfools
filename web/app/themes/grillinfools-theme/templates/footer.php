480bf4a214af99354abb0887e5a155890f3f803a
jasonbutler42 <jasonbutler42@gmail.com>
2015-01-16 17:17:12 -0600

    fix(sosweet): add nofollow
     - add rel=nofollow so Google does not get caught up in this whole mess

--- web/app/themes/grillinfools-theme/templates/footer.php
+++ web/app/themes/grillinfools-theme/templates/footer.php
@@ -2,6 +2,6 @@
   	<div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
   	</div>
-	<div class="tastytreats"><a href="http://grillinfools.com/dumbass_robots/sosweet.php">Bear</a></div>
+	<div class="tastytreats"><a rel="nofollow" href="http://grillinfools.com/dumbass_robots/sosweet.php">Bear</a></div>
   	<a href="/dumbass_robots/" rel="nofollow"><img src="/dumbass_robots/dumbass_robots.gif" alt=""></a>
 </footer>
