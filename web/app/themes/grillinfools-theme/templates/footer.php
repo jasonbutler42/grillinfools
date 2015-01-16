6fb76601ec7afc20bd5167104486094bd86e08a9
jasonbutler42 <jasonbutler42@gmail.com>
2015-01-16 16:34:03 -0600

    fix(dumbass_robots): bad bots
    - change order of honeypot so it comes before dumbass_robots. This should allow more bots to hit the honeypot before they get permabanned

--- web/app/themes/grillinfools-theme/templates/footer.php
+++ web/app/themes/grillinfools-theme/templates/footer.php
@@ -1,8 +1,7 @@
 <footer class="content-info" role="contentinfo">
   	<div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
-
   	</div>
-  	<a href="/dumbass_robots/" rel="nofollow"><img src="/dumbass_robots/dumbass_robots.gif?9c889a" alt=""></a>
 	<div style="display: none;"><a href="http://grillinfools.com/dumbass_robots/sosweet.php">Bear</a></div>
+  	<a href="/dumbass_robots/" rel="nofollow"><img src="/dumbass_robots/dumbass_robots.gif" alt=""></a>
 </footer>
