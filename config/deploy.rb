c3c34ee5f18ccb74e19db7f19bc132c2e5e2b1c1
jasonbutler42 <jasonbutler42@gmail.com>
2015-04-06 13:18:53 -0500

    test(link_dirs): switch order of directories and files

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,10 +15,10 @@ set :log_level, :info
 
 # Linked stuff gets automatically symbolically linked into the /current deployment folder from the shared folder
 # All files and folders are relative from webserver root (/current/)
-# Linked files
-set :linked_files, %w{.env web/.htaccess web/wp/.htaccess web/robots.txt web/dumbass_robots/blackhole.dat web/app/plugins/all-in-one-wp-security-and-firewall/logs/wp-security-log-cron-job.txt web/app/plugins/all-in-one-wp-security-and-firewall/logs/wp-security-log.txt}
 # Linked directories
 set :linked_dirs, %w{web/app/uploads web/app/wfcache web/app/w3tc-config web/app/cache web/app/aiowps_backups web/app/plugins/ad-injection-data web/app/plugins/fanciest-author-box web/app/plugins/pinterest-pin-it}
+# Linked files
+set :linked_files, %w{.env web/.htaccess web/wp/.htaccess web/robots.txt web/dumbass_robots/blackhole.dat web/app/plugins/all-in-one-wp-security-and-firewall/logs/wp-security-log-cron-job.txt web/app/plugins/all-in-one-wp-security-and-firewall/logs/wp-security-log.txt}
 
 namespace :deploy do
   desc 'Restart application'
