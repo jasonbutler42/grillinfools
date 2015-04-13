baffde5a76c1bc88eac5ce577df628ff72b4dbac
jasonbutler42 <jasonbutler42@gmail.com>
2015-04-12 19:34:17 -0500

    feature(footer.php): CCN Hat

--- web/app/themes/grillinfools-theme/templates/footer.php
+++ web/app/themes/grillinfools-theme/templates/footer.php
@@ -5,3 +5,9 @@
 	<div class="tastytreats"><a rel="nofollow" href="http://grillinfools.com/dumbass_robots/sosweet.php">Bear</a></div>
   	<a href="/dumbass_robots/" rel="nofollow"><img src="/dumbass_robots/dumbass_robots.gif" alt=""></a>
 </footer>
+<script>var cn_options = {jquery : true, site: "http://www.thedailymeal.com"};</script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js" type="text/javascript"></script>
+<link rel="stylesheet" type="text/css" href="http://www.thedailymeal.com/sites/all/modules/custom/default/cn/assets/tdm/cn.css" />
+<script src="http://www.thedailymeal.com/sites/all/modules/custom/default/cn/cn-load.js"></script>
+<script src='http://images.thedailymeal.net/sites/all/themes/thedailymeal/js/tdm_analytics.js' type='text/javascript'></script>
+<noscript><img src='http://b.scorecardresearch.com/p?c1=2&amp;c2=9009860&amp;cv=2.0&amp;cj=1'/><img height='1' src='https://ssl-thedailymeal-com-f54a04.c-col.com' width='1'/></noscript>
